2cbf6239beb9f4919a28831b4142f7a6f07bcb66
Osman Zakir <osmanzakir90@hotmail.com>
2022-03-14 22:58:58 +0500

    fixed remaining action name issues and indentation issues

--- src/components/contextApiComponents/ToDoContextApi.tsx
+++ src/components/contextApiComponents/ToDoContextApi.tsx
@@ -1,5 +1,5 @@
 import React, { useContext, useState, useEffect } from "react";
-import { ADD_TODO_ACTION, REMOVE_TODO_ACTION, UPDATE_TODO_ACTION } from "../../constants";
+import { ADD_ITEM_ACTION, REMOVE_ITEM_ACTION, UPDATE_ITEM_ACTION } from "../../constants";
 import ListFooter from "../ListFooter";
 import FilterReducer from "../../reducers/FilterReducer";
 import AddItem from "../AddItem";
@@ -9,56 +9,56 @@ import { ToDoContext } from "./ToDoProvider";
 const defaultListItem: ListItemProps = {id: Date.now(), text: "", completed: false};
 
 const ToDoContextApi:React.FC = () => {
-   const { state: { todoList }, dispatch } = useContext(ToDoContext);
-   const [listItem, setListItem] = useState(defaultListItem);
-   const [listData, setListData] = useState(todoList);
+  const { state: { todoList }, dispatch } = useContext(ToDoContext);
+  const [listItem, setListItem] = useState(defaultListItem);
+  const [listData, setListData] = useState(todoList);
 
-   useEffect(() => {
-     setListData(todoList);
-   }, [todoList]);
+  useEffect(() => {
+    setListData(todoList);
+  }, [todoList]);
 
-   const updateItem = (text:string) => {
-     setListItem({
-       id: Date.now(),
-       text,
-       completed: false
-     });
-   };
+  const updateItem = (text:string) => {
+    setListItem({
+      id: Date.now(),
+      text,
+      completed: false
+    });
+  };
 
-   const addItem = () => {
-     dispatch({
-       type: ADD_TODO_ACTION,
-       payload: listItem
-     });
-     setListItem(defaultListItem);
-   };
+  const addItem = () => {
+    dispatch({
+      type: ADD_ITEM_ACTION,
+      payload: listItem
+    });
+    setListItem(defaultListItem);
+  };
 
-   const removeItem = (id:number) => {
-     dispatch({
-       type: REMOVE_TODO_ACTION,
-       payload: { id }
-     });
-   };
+  const removeItem = (id:number) => {
+    dispatch({
+      type: REMOVE_ITEM_ACTION,
+      payload: { id }
+    });
+  };
 
-   const toggleItemStatus = (id:number) => {
-     dispatch({
-       type: UPDATE_TODO_ACTION,
-       payload: { id }
-     });
-   };
+  const toggleItemStatus = (id:number) => {
+    dispatch({
+      type: UPDATE_ITEM_ACTION,
+      payload: { id }
+    });
+  } ;
 
-   const filterList = (type:string) => {
-     const filteredList = FilterReducer(todoList, {type});
-     setListData(filteredList);
-   };
+  const filterList = (type:string) => {
+    const filteredList = FilterReducer(todoList, {type});
+    setListData(filteredList);
+  };
 
-   return (
-     <>
-       <AddItem item={listItem} updateItem={updateItem} addItem={addItem} />
-       <ToDoList data={listData} removeItem={removeItem} toggleItemStatus={toggleItemStatus} />
-       <ListFooter item={listData.length} storage="Context API" filterList={filterList} />
-     </>
-   );
+  return (
+    <>
+      <AddItem item={listItem} updateItem={updateItem} addItem={addItem} />
+      <ToDoList data={listData} removeItem={removeItem} toggleItemStatus={toggleItemStatus} />
+      <ListFooter item={listData.length} storage="Context API" filterList={filterList} />
+    </>
+  );
 };
 
 export default ToDoContextApi;
--- src/reducers/ToDoReducer.ts
+++ src/reducers/ToDoReducer.ts
@@ -1,5 +1,5 @@
 import { ListItemProps } from "../components/ToDoList";
-import { ADD_TODO_ACTION, REMOVE_TODO_ACTION, UPDATE_TODO_ACTION } from "../constants";
+import { ADD_ITEM_ACTION, REMOVE_ITEM_ACTION, UPDATE_ITEM_ACTION } from "../constants";
 
 interface ActionProps {
   type: string,
@@ -12,17 +12,16 @@ interface StateProps {
 
 const ToDoReducer = (state: StateProps = {todoList:[]}, action: ActionProps) => {
   switch (action.type) {
-    case ADD_TODO_ACTION:
+    case ADD_ITEM_ACTION:
       return { todoList: [...state.todoList, action.payload] };
-    case REMOVE_TODO_ACTION:
+    case REMOVE_ITEM_ACTION:
       return { 
         todoList: 
           state.todoList.length ? 
             state.todoList.filter(d => d.id !== action.payload.id ) : 
             [] 
-      }
-    ;
-    case UPDATE_TODO_ACTION:
+      };
+    case UPDATE_ITEM_ACTION:
       return {
         todoList:
           state.todoList.length ?
