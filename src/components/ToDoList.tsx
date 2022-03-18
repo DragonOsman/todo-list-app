3907fe2d1e01bbca27a35ec4e493733061891eef
Osman Zakir <osmanzakir90@hotmail.com>
2022-03-18 17:54:36 +0500

    removed avoidEscape key from quotes config, & edited ToDoList component and styling

--- src/components/ToDoList.tsx
+++ src/components/ToDoList.tsx
@@ -5,20 +5,20 @@ export interface ListItemProps {
   id: number,
   text: string,
   completed: boolean
-};
+}
 
 export interface ListProps {
   data: ListItemProps[],
   removeItem: (id:number) => void,
   toggleItemStatus: (id:number) => void
-};
+}
 
 const ToDoList:React.FC<ListProps> = ({ data, removeItem, toggleItemStatus }) => {
   return data.length > 0 ? (
     <div className="list-container">
       { data.map(dataItem => {
         return (
-          <ol key={dataItem.id}>
+          <ul key={dataItem.id}>
             <li>
               <div className="item-container">
                 <input 
@@ -26,18 +26,21 @@ const ToDoList:React.FC<ListProps> = ({ data, removeItem, toggleItemStatus }) =>
                   onChange={() => toggleItemStatus(dataItem.id)} 
                   checked={dataItem.completed}
                 />
-                <span className="item-text" style={{ textDecoration: dataItem.completed ? 
-                  "line-through" : "none" }}>{dataItem.text}</span>
+                <span className="item-text" style={{ textDecoration: dataItem.completed
+                  ? "line-through"
+                  : "none" }}>
+                {dataItem.text}
+                </span>
                 <button type="button" className="delete-button" onClick={() => removeItem(dataItem.id)}>
                   Delete
                 </button>
               </div>
             </li>
-          </ol>
-        )
+          </ul>
+        );
       }) }
     </div>
-  ) : (<p className="empty-list-note">No to-do items in list</p>);
+  ) : <p className="empty-list-note">No to-do items in list</p>;
 };
 
 export default ToDoList;
