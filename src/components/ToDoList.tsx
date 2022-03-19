755d23a0a2fbaa532a52cc9527051ef48b948047
Osman Zakir <osmanzakir90@hotmail.com>
2022-03-19 16:38:02 +0500

    tryiing to fix eslint problems

--- src/components/ToDoList.tsx
+++ src/components/ToDoList.tsx
@@ -13,37 +13,34 @@ export interface ListProps {
   toggleItemStatus: (id:number) => void
 }
 
-const ToDoList:React.FC<ListProps> = ({ data, removeItem, toggleItemStatus }) => {
-  return data.length > 0 ? (
-    <div className="list-container">
-      { data.map(dataItem => {
-        return (
-          <ul key={dataItem.id}>
-            <li>
-              <div className="item-container">
-                <input 
-                  type="checkbox" 
-                  onChange={() => toggleItemStatus(dataItem.id)} 
-                  checked={dataItem.completed}
-                />
-                <span className="item-text" style={{ textDecoration: dataItem.completed
-                  ? "line-through"
-                  : "none",
-                  backgroundColor: dataItem.completed
-                  ? "#8cbed6"
-                  : "#87ceeb" }}>
+const ToDoList:React.FC<ListProps> = ({ data, removeItem, toggleItemStatus }) =>
+  data.length > 0
+    ? (<div className="list-container">
+      { data.map(dataItem => (
+        <ul key={dataItem.id}>
+          <li style={{ backgroundColor: dataItem.completed
+            ? "#87ceeb"
+            : "#8cbed6" }}>
+            <div className="item-container">
+              <input
+                type="checkbox"
+                onChange={() => toggleItemStatus(dataItem.id)}
+                checked={dataItem.completed}
+              />
+              <span className="item-text" style={{ textDecoration: dataItem.completed
+                ? "line-through"
+                : "none" }}>
                 {dataItem.text}
-                </span>
-                <button type="button" className="delete-button" onClick={() => removeItem(dataItem.id)}>
-                  Delete
-                </button>
-              </div>
-            </li>
-          </ul>
-        );
-      }) }
+              </span>
+              <button type="button" className="delete-button" onClick={() => removeItem(dataItem.id)}>
+                Delete
+              </button>
+            </div>
+          </li>
+        </ul>
+      ))}
     </div>
-  ) : <p className="empty-list-note">No to-do items in list</p>;
-};
+    )
+    : <p className="empty-list-note">No to-do items in list</p>;
 
 export default ToDoList;
